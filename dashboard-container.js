7931b522e90b9a16f2ee8d1c8a2cf8d80d9b404c
seufster71 <seufster@gmail.com>
2022-05-18 16:35:43 -0400

    more setup

--- dashboard-container.js
+++ dashboard-container.js
@@ -4,19 +4,11 @@
 'use-strict';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate, useLocation } from "react-router-dom";
-import * as actions from '../../core/common/apppref-actions';
 import fuLogger from '../../core/common/fu-logger';
 import DashboardView from '../../memberView/dashboard/dashboard-view';
 
 function DashboardContainer() {
 	const itemState = useSelector((state) => state.memberdashboard);
-	const session = useSelector((state) => state.session);
-	const appMenus = useSelector((state) => state.appMenus);
-	const appPrefs = useSelector((state) => state.appPrefs);
-	const dispatch = useDispatch();
-	const location = useLocation();
-	const navigate = useNavigate();
 	
 
 	fuLogger.log({level:'TRACE',loc:'DashboardContainer::render',msg:"Hi there"});
